a32724ba5f3656a1a965c19619642f6742fc6d43
Drew Polhamus <nicolas.polhamus@gmail.com>
2022-10-09 00:25:11 -0600

    starts expanding logic for building mock field data

--- lib/createMockData.ts
+++ lib/createMockData.ts
@@ -16,14 +16,36 @@ export function createMockData(model: Model, recordAmount: number): Record<strin
     const currentRecord = mockRecords[currentRecordAmount];
     const currentField = fields[fieldIndex];
 
-    if (currentField.toLowerCase().includes('name')) {
-      currentRecord[currentField] = faker.name.firstName();
-    }
-
-    currentRecord[currentField] = faker.lorem.word();
+    currentRecord[currentField] = createDataForField(currentField); 
 
     fieldIndex++;
   }
 
   return mockRecords;
+}
+
+function createDataForField(currentField: string): string {
+  switch(true) {
+    case currentField.toLowerCase() === 'username':
+      return faker.internet.userName();
+    case currentField.toLowerCase().includes('name'): 
+      return hasFirstOrLastName(currentField) === 'first' ? 
+        faker.name.firstName() : faker.name.lastName();
+    case currentField.toLowerCase() === 'email':
+      return faker.internet.email();
+    case currentField.toLowerCase().includes('address'):
+      return faker.address.streetAddress(true);
+    case currentField.toLowerCase().includes('city'):
+      return faker.address.city();
+    case currentField.toLowerCase().includes('state'):
+      return faker.address.stateAbbr();
+    default:
+      return faker.lorem.word();
+  }
+}
+
+function hasFirstOrLastName(nameField: string): string {
+  const matches = [...nameField.toLowerCase().matchAll(/(first|last)(name)/g)];
+
+  return matches[1];
 }
